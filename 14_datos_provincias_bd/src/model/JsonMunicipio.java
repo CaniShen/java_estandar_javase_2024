e2b6050899dc0a1fc69f8e1e6746303278c41a74
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-05-06 11:02:44 +0200

    11

---
+++ 14_datos_provincias_bd/src/model/JsonMunicipio.java
@@ -0,0 +1,13 @@
+package model;
+
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JsonMunicipio {
+	public List <Municipio> municipios;
+}
